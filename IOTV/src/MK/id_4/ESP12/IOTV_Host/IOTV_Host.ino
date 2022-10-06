b7044ec21cd94ad766b79fe31004aa4794fadd1f
verbkinm <verbkinm@yandex.ru>
2022-10-06 14:08:40 +0500

    MK esp for id_4

---
+++ IOTV/src/MK/id_4/ESP12/IOTV_Host/IOTV_Host.ino
@@ -0,0 +1,163 @@
+#include <ESP8266WiFi.h>
+
+#include "IOTV_SH.h"
+#include "iot_server.h"
+
+#define BUFFSIZE 256
+
+// const char* ssid = "TP-Link_A6BE";
+// const char* password = "41706831";
+
+const char* ssid = "vm";
+const char* password = "12345678";
+
+WiFiServer server(8888);
+WiFiClient client;
+
+IOTV_Server iot;
+char recivedBuffer[BUFFSIZE] {0}, transmitBuffer[BUFFSIZE] {0};
+char *ptrBuf = recivedBuffer;
+
+WiFiEventHandler stationConnectedHandler;
+WiFiEventHandler stationDisconnectedHandler;
+
+void dataRecived();
+void debug();
+
+void setup() 
+{
+  stationConnectedHandler = WiFi.onStationModeConnected(&onStationConnected);
+  stationDisconnectedHandler = WiFi.onStationModeDisconnected(&onStationDisconnected);
+
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(500);
+  }
+
+  server.begin();
+}
+
+void loop() 
+{
+  if (!client.connected()) 
+  {
+    client.flush();
+    ptrBuf = recivedBuffer;
+    
+    client = server.available();
+    if (client)
+      Serial.println("Connected client: " + client.remoteIP().toString() + ":" + client.remotePort());
+  } 
+  else 
+  {
+    while (client.available()) 
+      void dataRecived();
+      // Serial.print(client.read(), HEX);
+  }
+
+  // if (WiFi.status() != WL_CONNECTED)
+  // {
+  //   client.flush();
+  //   ptrBuf = recivedBuffer;
+
+  //   Serial.println("Wifi not connect");
+  //   Serial.print("Connecting");
+  //   while (WiFi.status() != WL_CONNECTED)
+  //   {
+  //     Serial.print('.');
+  //     delay(500);
+  //   }
+  // }
+}
+
+void dataRecived() 
+{
+  uint16_t dataSize = 0;
+  *ptrBuf = (char)client.read();
+  // ptrBuf++;
+
+  // if (ptrBuf == (recivedBuffer + BUFFSIZE))
+  //   ptrBuf = recivedBuffer;
+
+  // while (ptrBuf != recivedBuffer) 
+  // {
+  //   if (recivedBuffer[0] == Protocol_class::QUERY_WAY_BYTE) 
+  //   {
+  //     dataSize = Protocol_class::response_WAY(iot, transmitBuffer);
+  //     client.write(transmitBuffer, dataSize);
+  //     memmove((void*)recivedBuffer, (void*)&recivedBuffer[1], BUFFSIZE - 1);
+  //     ptrBuf--;
+  //   } 
+  //   else if ((recivedBuffer[0] & 0x0F) == Protocol_class::QUERY_READ_BYTE) 
+  //   {
+  //     dataSize = Protocol_class::response_READ(iot, recivedBuffer, ptrBuf, transmitBuffer);
+  //     client.write(transmitBuffer, dataSize);
+  //     memmove((void*)recivedBuffer, (void*)&recivedBuffer[1], BUFFSIZE - 1);
+  //     ptrBuf--;
+  //   } 
+  //   else if (recivedBuffer[0] == Protocol_class::QUERY_PING_BYTE) 
+  //   {
+  //     dataSize = Protocol_class::response_Pong(transmitBuffer);
+  //     client.write(transmitBuffer, dataSize);
+  //     memmove((void*)recivedBuffer, (void*)&recivedBuffer[1], BUFFSIZE - 1);
+  //     ptrBuf--;
+  //   } 
+  //   else if ((recivedBuffer[0] & 0x0F) == Protocol_class::QUERY_WRITE_BYTE) 
+  //   {
+  //     // лоакальный dataSize, так как response_WRITE может вернуть -1
+  //     int dataSize = Protocol_class::response_WRITE(iot, recivedBuffer, ptrBuf, transmitBuffer);
+  //     if (dataSize >= 0) 
+  //     {
+  //       client.write(transmitBuffer, 1);  // ответ на write = 1 байт
+  //       memmove((void*)recivedBuffer, (void*)&recivedBuffer[dataSize + 3], BUFFSIZE - (dataSize + 3));
+  //       ptrBuf -= dataSize + 3;
+  //     }
+  //   } 
+  //   else
+  //     ptrBuf = recivedBuffer;
+
+  //   if (ptrBuf < recivedBuffer)
+  //     ptrBuf = recivedBuffer;
+
+  //   debug();
+  // }
+}
+
+void debug() 
+{
+  Serial.print("Play: " + iot.play());
+  Serial.print(", Led: ");
+  Serial.print(iot.led());
+  Serial.print(", Repeate: ");
+  Serial.print(iot.repeate());
+  Serial.print(", Mode: ");
+  Serial.println(iot.mode());
+  // Serial.print("Play: " + iot.play());
+  // Serial.print(", Led: " + iot.led());
+  // Serial.print(", Repeate: " + iot.repeate());
+  // Serial.print(", Mode: " + iot.mode());
+}
+
+void onStationConnected(const WiFiEventStationModeConnected& evt) 
+{
+  Serial.print("Station connected: ");
+  Serial.println(evt.ssid);
+}
+
+void onStationDisconnected(const WiFiEventStationModeDisconnected& evt) 
+{
+  Serial.print("Station disconnected: ");
+  Serial.println(evt.ssid);
+  Serial.print("Code disconnected: ");
+  Serial.println(evt.reason);
+
+  client.flush();
+  ptrBuf = recivedBuffer;
+}
---
+++ IOTV/src/MK/id_4/ESP12/IOTV_Host/iot_server.cpp
@@ -0,0 +1,58 @@
+#include "iot_server.h"
+
+IOTV_Server::IOTV_Server() : _id(4), _description("Облачко для любимой доченьки"),
+    _play(_readChannel[0]), _led(_readChannel[1]), _repeate(_readChannel[2]), _mode(_readChannel[3])
+{
+    std::memset(_readChannel, 0, READ_CHANNEL_LENGTH);
+
+    std::memset(_readChannelType, Protocol_class::DATA_TYPE::BOOL, READ_CHANNEL_LENGTH - 1);
+    std::memset(_writeChannelType, Protocol_class::DATA_TYPE::BOOL, WRITE_CHANNEL_LENGTH - 1);
+    // for (uint8_t i = 0; i < READ_CHANNEL_LENGTH - 1; i++)
+    //     _readChannelType[i] = Protocol_class::DATA_TYPE::BOOL;
+
+    // for (uint8_t i = 0; i < WRITE_CHANNEL_LENGTH - 1; i++)
+    //     _writeChannelType[i] = Protocol_class::DATA_TYPE::BOOL;
+
+    _readChannelType[3] = Protocol_class::DATA_TYPE::INT_8;
+    _writeChannelType[3] = Protocol_class::DATA_TYPE::INT_8;
+}
+
+bool IOTV_Server::play() const
+{
+    return _play;
+}
+
+void IOTV_Server::setPlay(bool newPlay)
+{
+    _play = newPlay;
+}
+
+bool IOTV_Server::led() const
+{
+    return _led;
+}
+
+void IOTV_Server::setLed(bool newLed)
+{
+    _led = newLed;
+}
+
+bool IOTV_Server::repeate() const
+{
+    return _repeate;
+}
+
+void IOTV_Server::setRepeate(bool newRepeate)
+{
+    _repeate = newRepeate;
+}
+
+uint8_t IOTV_Server::mode() const
+{
+    return _mode;
+}
+
+void IOTV_Server::setMode(uint8_t newMode)
+{
+    _mode = newMode;
+}
