838cdb57687cc0f6598989b6ca3869474d7e9b95
user <user@lenovo.loc>
2023-02-03 11:10:20 +0500

    MK id 4

--- IOTV/src/MK/Arduino/id_4/ESP12_2/IOTV_Host/IOTV_Host.ino
+++ IOTV/src/MK/Arduino/id_4/ESP12_2/IOTV_Host/IOTV_Host.ino
@@ -5,7 +5,7 @@
 #include <iotv_server_embedded.h>
 
 #define PLAY_PIN D5
-#define LED_PIN D7
+// #define LED_PIN D7
 #define MODE_1_PIN D1
 #define MODE_2_PIN D6
 #define MODE_3_PIN D2
@@ -45,28 +45,33 @@ bool error = false;
 const uint8_t NUMBER_READ_CHANNEL = 4;
 const uint8_t NUMBER_WRITE_CHANNEL = NUMBER_READ_CHANNEL;
 
-uint8_t readType[NUMBER_READ_CHANNEL] = {DATA_TYPE_INT_32, DATA_TYPE_BOOL, DATA_TYPE_INT_8, DATA_TYPE_BOOL};
-uint8_t *writeType = readType;
-
+const uint8_t readType[NUMBER_READ_CHANNEL] = { 
+                                                DATA_TYPE_INT_16, 
+                                                DATA_TYPE_BOOL, 
+                                                DATA_TYPE_INT_8, 
+                                                DATA_TYPE_BOOL
+                                              };
 struct IOTV_Server_embedded iot = {
     .id = 4,
     .name = "Cloud",
     .description = "Облачко - музыкальный ночник для Евы-Королевы",
     .numberReadChannel = NUMBER_READ_CHANNEL,
     .readChannel = NULL,
-    .readChannelType = readType,
+    .readChannelType = (uint8_t *)readType,
     .numberWriteChannel = NUMBER_WRITE_CHANNEL,
-    .writeChannelType = writeType,
+    .writeChannelType = (uint8_t *)readType,
     .state = 1,
     .nameSize = static_cast<uint8_t>(strlen(iot.name)),
     .descriptionSize = static_cast<uint8_t>(strlen(iot.description)),
 };
 
+//               IOTVP_embedded
 //**************************************************
 
 void dataRecived(char ch);
 uint8_t avrADC();
 void connectToWifi();
+void debug();
 
 WiFiEventHandler stationConnectedHandler;
 WiFiEventHandler stationDisconnectedHandler;
@@ -79,12 +84,10 @@ void setup()
 
   // Настройка портов ввода/вывода
   pinMode(PLAY_PIN, INPUT);
-  pinMode(LED_PIN, OUTPUT);
   pinMode(MODE_1_PIN, INPUT);
   pinMode(MODE_2_PIN, INPUT);
   pinMode(MODE_3_PIN, INPUT);
 
-  digitalWrite(LED_PIN, HIGH);
   digitalWrite(MODE_1_PIN, LOW);
   digitalWrite(MODE_2_PIN, LOW);
   digitalWrite(MODE_3_PIN, LOW);
@@ -108,7 +111,7 @@ void setup()
   for (uint8_t i = 0; i < iot.numberReadChannel; ++i)
   {
     iot.readChannel[i].dataSize = dataSizeonDataType(readType[i]);
-    iot.readChannel[i].data = (char *)malloc(iot.readChannel[i].dataSize);
+    iot.readChannel[i].data = (char *)calloc(iot.readChannel[i].dataSize, sizeof(char));
   }
 }
 
@@ -129,35 +132,20 @@ void loop()
   if (WiFi.status() != WL_CONNECTED) 
     connectToWifi();
 
-
-  // Записываем сренее значение ADC в канал чтения
-  auto resADC = avrADC();
+  // Записываем сренее значение ADC в канал чтения ADC
+  int16_t resADC = avrADC();
   memcpy(iot.readChannel[ADC].data, &resADC, iot.readChannel[ADC].dataSize);
 
-  // Если установлен флаг повтора и мелодия не воспроизводится
-  !!!
-  if (iot.readChannel[REPEATE].data && !(resADC > ADC_BORDER)) // iot.readChannel[ADC] > ADC_BORDER) - идёт воспроизведение
-  {
-    // Делаем задержку 
-    delay(3000);
-
-    // И ещё раз проверяем состояние воспроизведения, если мелодия не воспроизводится, устанавливаем флаг переключения состояния воспроизведения
-    resADC = avrADC();
-    memcpy(iot.readChannel[ADC].data, &resADC, iot.readChannel[ADC].dataSize);
-    if (resADC < ADC_BORDER)
-      *(iot.readChannel[TRIGGER].data) = true;
-  }
-
   // Если установлен флаг переключения состояния воспроизведения
-  if (iot.readChannel[TRIGGER]) 
+  if (*(bool *)(iot.readChannel[TRIGGER].data)) 
   {
     // Подаём логическую единицу на ножку PLAY_PIN
     pinMode(PLAY_PIN, OUTPUT);
     digitalWrite(PLAY_PIN, HIGH);
     
-    // Если мелодия воспроизводится, делаем задержку на 3500 мс (для выключения), иначе на 500 мс
-    if (iot.readChannel[ADC] > ADC_BORDER)
-      delay(3500);
+    // Если мелодия воспроизводится, делаем задержку на 3000 мс (для выключения), иначе на 500 мс для включения
+    if (*(int16_t *)iot.readChannel[ADC].data > ADC_BORDER)
+      delay(3000);
     else
       delay(500);
 
@@ -165,27 +153,40 @@ void loop()
     digitalWrite(PLAY_PIN, LOW);
     pinMode(PLAY_PIN, INPUT);
 
-    // Снимаем флаг 
-    iot.readChannel[TRIGGER] = false;
+    // Снимаем флаг переключения состояния воспроизведения
+    *(bool *)iot.readChannel[TRIGGER].data = false;
   } 
-  
-  // Если установлен режим отличный от -1
-  if ((iot.readChannel[MODE] > -1) && (iot.readChannel[MODE] < 3)) 
+  // Если установлен режим в диапазоне -1 < mode < 3
+  else if ((*(int8_t *)iot.readChannel[MODE].data > -1) && (*(int8_t *)iot.readChannel[MODE].data < 3)) 
   {
-    Serial.print("set mode: ");
-    Serial.println(iot.readChannel[MODE]);
-
     // Устанавливаем логическую единицу на ножке соответствующей переданному режиму
-    pinMode(modeArr[iot.readChannel[MODE]], OUTPUT);
-    digitalWrite(modeArr[iot.readChannel[MODE]], HIGH);
+    pinMode(modeArr[*(uint8_t *)iot.readChannel[MODE].data], OUTPUT);
+    digitalWrite(modeArr[*(uint8_t *)iot.readChannel[MODE].data], HIGH);
     delay(500);
     // Устанавливаем логический ноль на ножке соответствующей переданному режиму и переключаем режим на INPUT
-    digitalWrite(modeArr[iot.readChannel[MODE]], LOW);
-    pinMode(modeArr[iot.readChannel[MODE]], INPUT);
+    digitalWrite(modeArr[*(uint8_t *)iot.readChannel[MODE].data], LOW);
+    pinMode(modeArr[*(uint8_t *)iot.readChannel[MODE].data], INPUT);
 
     // Убираем присутствие переключения режима
-    iot.readChannel[MODE] = -1;
+    *iot.readChannel[MODE].data = -1;
   }
+  // Если установлен флаг повтора и мелодия не воспроизводится
+  else if (*(bool *)iot.readChannel[REPEATE].data && (*(int16_t *)iot.readChannel[ADC].data < ADC_BORDER)) // iot.readChannel[ADC].data > ADC_BORDER) - идёт воспроизведение
+  {
+    static uint8_t repeateCounter;
+    ++repeateCounter;
+    // Делаем задержку 
+    delay(1000);
+
+    if (repeateCounter > 4)
+    {
+      *(bool *)(iot.readChannel[TRIGGER].data) = true;
+      repeateCounter = 0;
+    }
+  }
+
+  // delay(500);
+  // debug();
 }
 
 void dataRecived(char ch)
@@ -293,4 +294,16 @@ void connectToWifi()
     delay(500);
   }
   Serial.print("\n");
+}
+
+void debug() 
+{
+  Serial.print("CH0 (ADC): ");
+  Serial.println(*(int16_t *)iot.readChannel[ADC].data);
+  Serial.print("CH1, (Repeate): ");
+  Serial.print(*(bool *)iot.readChannel[REPEATE].data);
+  Serial.print("CH2 (Mode): ");
+  Serial.println(*(int8_t *)iot.readChannel[MODE].data);
+  Serial.print("CH3 (Trigger): ");
+  Serial.println(*(bool *)iot.readChannel[TRIGGER].data);
 }
