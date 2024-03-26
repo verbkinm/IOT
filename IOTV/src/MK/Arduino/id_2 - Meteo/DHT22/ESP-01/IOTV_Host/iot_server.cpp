d56ef7d4141ae41a4f666b1669b5f616b707d405
verbkinm <verbkinm@yandex.ru>
2024-03-26 13:35:32 +0500

    удаление старых файлов

--- IOTV/src/MK/Arduino/id_2 - Meteo/DHT22/ESP-01/IOTV_Host/iot_server.cpp
+++
@@ -1,8 +0,0 @@
-#include "iot_server.h"
-
-IOT_Server::IOT_Server() : _id(2), _description("DHT22-01")
-{
-    _readChannelType[0] = Raw::DATA_TYPE::FLOAT_32;
-    _readChannelType[1] = Raw::DATA_TYPE::FLOAT_32;
-    //_readChannelType[2] = Raw::DATA_TYPE::FLOAT_32;
-}
