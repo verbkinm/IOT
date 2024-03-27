20906b2ef61e205bed04bf708d0712941c7b2843
verbkinm <verbkinm@yandex.ru>
2024-03-27 16:55:04 +0500

    IOTV_server 0.11.1

--- IOTV/src/IOTV_server/main.cpp
+++ IOTV/src/IOTV_server/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    a.setApplicationVersion("0.11");
+    a.setApplicationVersion("0.11.1");
 
     Log::write("Start program IOTV_Server " + QString(a.applicationVersion()),
                Log::Write_Flag::FILE_STDOUT,
